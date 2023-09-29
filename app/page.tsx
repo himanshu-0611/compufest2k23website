c289f760330e8a61dc43d8334b4f164bb70dad47
Compufest Technical <108852991+compufestycce@users.noreply.github.com>
2023-09-29 21:25:47 +0530

    deleted event slider from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,19 +78,15 @@ export default function Home() {
             </div>
 
             <div>
-              <h1
-                className="text-4xl flex justify-center font-bold text-white lg:pt-28"
-              >
-                Events
-              </h1>
+              
 
-              <EventSlider />
+             
               <div className="justify-center flex py-10" >
                 <button
                   type="button"
                   className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-xl px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                 >
-                  <Link href="/events">More Events</Link>
+                  <Link href="/events">Checkout Events</Link>
                 </button>
               </div>
             </div>
