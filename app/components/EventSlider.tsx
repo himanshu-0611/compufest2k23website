f45c0c8e40d9fe102929bdb68b852e21b26d30ff
KaKashi1210 <desmondaditya@gmail.com>
2023-09-29 18:51:22 +0530

    slider fix

--- app/components/EventSlider.tsx
+++ app/components/EventSlider.tsx
@@ -32,46 +32,14 @@ export default function EventSlider() {
         <SwiperSlide >
           <div className="slide-content ">
             <a href="/events">
-              <img src="assets/Flyers/arduino.jpg" className="glow"/>
+              <img src="assets/Flyers/arduino.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
             </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="slide-content">
             <a href="/events">
-              <img src="assets/Flyers/uiux.jpg" className="glow"/>
-            </a>
-          </div>
-        </SwiperSlide>
-
-        
-
-        <SwiperSlide>
-          <div className="slide-content">
-            <a href="/events">
-              <img src="assets/Flyers/webnetic.jpg" className="glow"/>
-            </a>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="slide-content">
-            <a href="/events">
-              <img src="assets/Flyers/computingtechnology.jpg" className="glow"/>
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <a href="/events">
-              <img src="assets/Flyers/optimizer.jpg" className="glow"/>
-            </a>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-            <a href="/events">
-              <img src="assets/Flyers/web3.jpg" className="glow"/>
+              <img src="assets/Flyers/uiux.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
             </a>
           </div>
         </SwiperSlide>
@@ -81,7 +49,39 @@ export default function EventSlider() {
         <SwiperSlide>
           <div className="slide-content">
             <a href="/events">
-              <img src="assets/Flyers/powerbi.jpg" className="glow"/>
+              <img src="assets/Flyers/webnetic.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
+            </a>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="slide-content">
+            <a href="/events">
+              <img src="assets/Flyers/computingtechnology.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <a href="/events">
+              <img src="assets/Flyers/optimizer.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-content">
+            <a href="/events">
+              <img src="assets/Flyers/web3.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
+            </a>
+          </div>
+        </SwiperSlide>
+
+        
+
+        <SwiperSlide>
+          <div className="slide-content">
+            <a href="/events">
+              <img src="assets/Flyers/powerbi.jpg" className="glow h-[15rem] lg:h-96 w-fit"/>
             </a>
           </div>
         </SwiperSlide> 
